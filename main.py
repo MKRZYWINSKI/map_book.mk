ff0283b9c301bc390f55a46f4674427a7413ab7d
MKRZYWINSKI <167193624+MKRZYWINSKI@users.noreply.github.com>
2024-04-23 14:25:54 +0200

    feat: added a create user function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from models.data import users
-from init.crud import read
+from init.crud import read,add_user
+
 
 if __name__ == '__main__':
     print(f'witaj {users[0]["name"]}')
 
 
 
+    read(users)
+    add_user(users)
     read(users)
 
 
