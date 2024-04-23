e0d6e5d39db249cb6e134d7cba46424015847bf3
MKRZYWINSKI <167193624+MKRZYWINSKI@users.noreply.github.com>
2024-04-23 15:18:34 +0200

    dodano usuwanie

--- init/crud.py
+++ init/crud.py
@@ -8,5 +8,14 @@ def add_user(lista: list) -> None:
     user_post = input('Podaj ile postów:')
     lista.append({'name': user_name, 'surname': user_surname, 'post': user_post}, )
 
+    def remove_user(users: list[dict[str, str]]) -> None:
+        user_name = input('kogo skasować')
+        for user in users[1:]:
+            if user['name'] == user_name:
+                print(f'zanloziono xnajmowego{user['name']}  ')
+                users.remove(user)
+
+    remove_user(users)
+
 
 
--- main.py
+++ main.py
@@ -7,8 +7,19 @@ if __name__ == '__main__':
 
 
 
-    read(users)
-    add_user(users)
-    read(users)
+while True:
+        print('0.zakoncz program')
+        print('1.wyswietl uzytkownikow: ')
+        print('2.wyswiatl znajmych')
+        print('3.wyswietl')
+        print('4.usun znajmowego')
+        menu_option=input('wybierz opcje menu')
+        if menu_option == '0':break
+        if menu_option == '1':read(users)
+        if menu_option == '2':add_user(users)
+        if menu option == "3": search(users)
+        if menu option == "4": remove.user(users)
+
+
 
 
